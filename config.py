0b969a9bdd1fcdceadcf4d032aa6ed4738c1745b
Bagdasha <74656394+CoderBogdasha@users.noreply.github.com>
2021-02-19 19:56:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ students = ['SURNAME NAME', 'SURNAME NAME', 'SURNAME NAME' ...]
 color = 0x######
 bot_activity = 'STATUS'
 edit_role = 'ROLE_WHICH_CAN_CREATE_AND_EDIT_HOMEWORKS'
+prefix = 'COMMAND_PREFIX(/, !, ?...)'
