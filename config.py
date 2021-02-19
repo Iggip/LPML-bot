c38ffc97a897bc5c0573ee69d2faf9c2762d8a7e
Bagdasha <74656394+CoderBogdasha@users.noreply.github.com>
2021-02-19 19:53:25 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "YOUR_TOKEN"
+way = 'YOUR_FILE_WHERE_HOMEWORK_WILL_BE_STORED.dat'
+students = ['SURNAME NAME', 'SURNAME NAME', 'SURNAME NAME' ...]
+color = 0x######
+bot_activity = 'STATUS'
+edit_role = 'ROLE_WHICH_CAN_CREATE_AND_EDIT_HOMEWORKS'
