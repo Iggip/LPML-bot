f62ee128ce09a5ce0a6868e52e5cdc91a81d3d3d
Bagdasha <74656394+CoderBogdasha@users.noreply.github.com>
2021-02-19 18:45:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-TOKEN = "ODA0NjgxMDQ5OTc1OTQ3Mjk0.YBP3oA.9Ib_T1dZzTC2siO0W4x6El5eqnQ"
-list_way = 'C:\\Users\\moret\\Documents\\Python\\discord\\homework_list.dat'
-way = 'C:\\Users\\moret\\Documents\\Python\\discord\\homework.dat'
+TOKEN = "YOUR_TOKEN"
+way = 'YOUR_FILE_WHERE_HOMEWORK_WILL_BE_STORED'
